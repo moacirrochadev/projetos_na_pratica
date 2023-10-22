f74801082acb03211afff9b6d4b56a9d9caeb323
moacirrochadev <empreendedormoacirneto@gmail.com>
2023-10-22 18:58:49 -0300

    first commit

---
+++ src/main/java/com/desafio/demo/DesafioHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioHelloWorldApplication.class, args);
+	}
+
+}
